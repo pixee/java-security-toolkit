b93b8064c0689bccd2c51bdc36daf148a3accfb8
Arshan Dabirsiaghi <arshan@pixee.ai>
2022-05-06 16:26:50 -0400

    code cov

--- src/test/java/io/pixee/security/JakartaTest.java
+++ src/test/java/io/pixee/security/JakartaTest.java
@@ -2,8 +2,10 @@ package io.pixee.security;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -24,6 +26,7 @@ final class JakartaTest {
         "WEB-INF/classes/whatever.xml",
         "WEB-INF/lib/whatever.xml",
         "WEB-INF/./web.xml",
+        "WEB-INF/test" + (char) 0x00
       })
   void it_protects_against_attacks(String safePath) {
     assertThrows(SecurityException.class, () -> Jakarta.validateForwardPath(safePath));
@@ -32,6 +35,7 @@ final class JakartaTest {
   @ParameterizedTest
   @ValueSource(
       strings = {
+        "test",
         "fine/WEB-INF/foo.jsp",
         "fine/WEB-INF\\jsps\\foo.jsp",
         "/WEB-INF/good.xsd",
@@ -39,5 +43,10 @@ final class JakartaTest {
       })
   void it_allows_normal_paths(String normalPath) {
     assertThat(Jakarta.validateForwardPath(normalPath), is(normalPath));
+  }
+
+  @Test
+  void it_passes_null() {
+    assertThat(Jakarta.validateForwardPath(null), is(nullValue()));
   }
 }
