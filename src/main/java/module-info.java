24b1a5b9192963169c08ad7c2f00d3811999949e
Arshan Dabirsiaghi <arshan.dabirsiaghi@gmail.com>
2023-09-21 00:05:17 -0400

    wip

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/** Export our package so that it can be used by other modules. */
+open module io.github.pixee.security {
+  exports io.github.pixee.security;
+
+  requires org.apache.commons.io;
+  requires java.xml;
+  requires java.desktop;
+  requires java.base;
+}
