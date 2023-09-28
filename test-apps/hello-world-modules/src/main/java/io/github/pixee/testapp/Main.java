c01da23f7ab80b7c2c7dfe7f70c6a769576e22a3
Ryan Dens <ryandens735@gmail.com>
2023-09-28 12:34:20 -0700

    Update test-apps/hello-world-modules/src/main/java/io/github/pixee/testapp/Main.java
    Co-authored-by: Arshan Dabirsiaghi <arshan.dabirsiaghi@gmail.com>

--- test-apps/hello-world-modules/src/main/java/io/github/pixee/testapp/Main.java
+++ test-apps/hello-world-modules/src/main/java/io/github/pixee/testapp/Main.java
@@ -3,7 +3,7 @@ package io.github.pixee.testapp;
 import io.github.pixee.security.HostValidator;
 
 public final class Main {
-
+/** A simple piece of code that references the library, so we know the module visibility is correct. */
     public static void main(final String[] args) {
         String message = "Hello, World!";
         if (HostValidator.DENY_COMMON_INFRASTRUCTURE_TARGETS.isAllowed(message)) {
