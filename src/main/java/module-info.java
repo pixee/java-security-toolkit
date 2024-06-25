5d72fe7350b86d986ae3e3b03c2ad9387a5c366f
Arshan Dabirsiaghi <arshan.dabirsiaghi@gmail.com>
2024-06-25 10:33:25 -0400

    add module import

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ open module io.github.pixee.security {
 
     requires org.apache.commons.io;
     requires java.xml;
+    requires java.naming;
     requires java.desktop;
     requires java.base;
 }
